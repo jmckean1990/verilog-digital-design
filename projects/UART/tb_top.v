c6d4fdf082812012d2b0f8052e22faa46d1ff3c2
jmckean1990 <jmckean2925@gmail.com>
2024-09-23 07:31:51 -0400

    Modified clock divider, transmitter, and testbench to handle and test at a 100MHz clock(board clock speed)

--- projects/UART/tb_top.v
+++ projects/UART/tb_top.v
@@ -1,23 +1,23 @@
 `timescale 1ns / 1ps
 
 module tb_top();
-    reg clk_1MHz, rst, load;
+    reg clk_100MHz, rst, load;
     reg [7:0] inData;
     wire [7:0] outData;
     
-    top t(clk_1MHz, rst, load, inData, outData);
+    top t(clk_100MHz, rst, load, inData, outData);
     
-    always #500 clk_1MHz = ~clk_1MHz;
+    always #5 clk_100MHz = ~clk_100MHz;
     
     initial begin
-        clk_1MHz = 0;
+        clk_100MHz = 0;
         load = 0;
         inData = 8'b10010110;
         
         rst = 1;
         #(2* 104 * 1000) rst = 0;
         
-        load = 1;
+        #(104 * 1000) load = 1;
         #(104 * 1000) load = 0;
         #(10 * 104 * 1000);
         
--- projects/UART/top.v
+++ projects/UART/top.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
 module top(
-    input clk_1MHz, rst, load,
+    input clk_100MHz, rst, load,
     input [7:0] inData,
     output [7:0] outData
 );
     wire bitValue, clock_baud_9600;
     
-    clock_divider #(52) cd(clk_1MHz, rst, clock_baud_9600);
+    clock_divider #(5208) cd(clk_100MHz, rst, clock_baud_9600);
     uart_transmitter ut(clock_baud_9600, rst, load, inData, bitValue);
-    uart_receiver ur(clk_1MHz, rst, bitValue, outData);
+    uart_receiver ur(clk_100MHz, rst, bitValue, outData);
 endmodule
